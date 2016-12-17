859038f0dd4f5e5df81c108a15779f9ae28c05db
sarawut kodkeaw <sirguys.apple@gmail.com>
2016-12-17 19:49:21 +0700

    update profile component

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    Profile
+  <div class="ui segment">
+    <h3 class="ui header">Profile</h3>
+    <h4>Name: </h4> {{profile.name}}
+    <h4>Description: </h4> {{profile.description}}
+    <br><br>
+    <router-link to="edit" append class="ui green button">Edit</router-link>
   </div>
 </template>
+
+<script>
+  import firebase from 'firebase'
+
+  export default {
+    data: () => ({
+      profile: {
+        name: '',
+        description: ''
+      }
+    }),
+    created () {
+      const userId = firebase.auth().currentUser.uid
+      firebase.database()
+        .ref(`user/${userId}`)
+        .once('value', (snapshot) => {
+          this.profile = snapshot.val()
+        })
+    }
+  }
+</script>
