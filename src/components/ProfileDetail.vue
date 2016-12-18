b6d4b38c823666ccb1602b7f64924dd141c49c56
sarawut kodkeaw <sirguys.apple@gmail.com>
2016-12-18 18:05:41 +0700

    upload image to firebase storage

--- src/components/ProfileDetail.vue
+++ src/components/ProfileDetail.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
+    <img v-if="profile" :src="profile.photo" class="ui small circular image">
     <h4>Name: </h4> {{profile.name | upper}}
     <h4>Description: </h4> {{profile.description}}
   </div>
 </template>
+
+<style scoped>
+  img.circular {
+    width: 180px;
+    height: 180px;
+  }
+</style>
 
 <script>
   export default {
--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -1,6 +1,11 @@
 <template>
   <form class="ui form" @submit.prevent="save">
     <div class="field">
+      <label>Photo</label>
+      <img v-if="photo" :src="photo" class="ui small circular image">
+      <div @click="openUpload" class="ui green button">Upload Photo</div>
+    </div>
+    <div class="field">
       <label>Name</label>
       <input v-model="name">
     </div>
@@ -10,35 +15,58 @@
     </div>
     <button class="ui submit blue button">Save</button>
     <div class="ui red button" @click="$emit('cancle')">Cancle</div>
+    <UploadModal ref="upload" @success="uploaded"/>
   </form>
 </template>
 
+<style scoped>
+  img.circular {
+    width: 120px;
+    height: 120px;
+  }
+</style>
+
 <script>
+  import UploadModal from './UploadModal'
+
   export default {
+    components: {
+      UploadModal
+    },
     props: ['value'],
     data: () => ({
       name: '',
-      description: ''
+      description: '',
+      photo: ''
     }),
     created () {
       console.log(this.value)
       this.name = this.value.name
       this.description = this.value.description
+      this.photo = this.value.photo
     },
     watch: {
       value () {
         console.log(this.value)
         this.name = this.value.name
         this.description = this.value.description
+        this.photo = this.value.photo
       }
     },
     methods: {
       save () {
         this.$emit('input', {
           name: this.name,
-          description: this.description
+          description: this.description,
+          photo: this.photo
         })
         this.$emit('save')
+      },
+      openUpload () {
+        this.$refs.upload.open()
+      },
+      uploaded (url) {
+        this.photo = url
       }
     }
   }
